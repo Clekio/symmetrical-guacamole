bd5444530c260844680057e71654c3ff5eac81c1
Clekio <alvaro.ortuno.m@gmail.com>
2017-11-02 13:19:45 +0100

    sound

---
+++ Assets/Scripts/Alvaro/Music.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+[RequireComponent(typeof(AudioSource))]
+
+public class Music : MonoBehaviour {
+
+
+    
+
+    void Awake()
+    {
+
+
+            AudioSource audio = GetComponent<AudioSource>();
+            audio.Play();
+           
+
+        
+
+    }
+
+
+
+
+
+
+
+
+}
