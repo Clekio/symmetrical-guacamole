78d6597189aa603137b1f14bf21e2d3ca8e87872
Clekio <alvaro.ortuno.m@gmail.com>
2017-11-02 04:28:38 +0100

    luces

---
+++ Assets/Scripts/Alvaro/CambioCamara.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CambioCamara : MonoBehaviour {
+
+    public GameObject camera1;
+    public GameObject camera2;
+
+    private void OnTriggerEnter(Collider Player)
+    {
+        camera1.SetActive(false);
+        camera2.SetActive(true);
+
+    }
+
+    private void OnTriggerExit(Collider Player)
+    {
+        camera1.SetActive(true);
+        camera2.SetActive(false);
+
+    }
+
+}
---
+++ Assets/Scripts/Alvaro/LightManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightManager : MonoBehaviour {
+
+
+    public GameObject bloque;
+    public GameObject bloque2;
+
+    //public Light luz1;
+    //public Light luz2;
+    //public Light luz3;
+    //public Light luz4;
+    //public Light luz5;
+
+
+    private void OnTriggerExit(Collider Player)
+    {
+
+        //luz1.enabled = !luz1.enabled;
+
+        StartCoroutine(Count0());
+
+        //bloque.SetActive(true);
+
+    }
+
+
+    IEnumerator Count0()
+    {
+
+
+        yield return new WaitForSeconds(0.3f);
+
+        bloque.SetActive(true);
+        bloque2.SetActive(false);
+
+        yield return null;
+
+    }
+
+}
