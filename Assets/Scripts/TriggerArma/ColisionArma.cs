9f52d4ac3dfd6319614cccce65f66a7c63c1726a
Clekio <alvaro.ortuno.m@gmail.com>
2017-11-22 12:35:05 +0100

    Sonidos
    Sonidos

---
+++ Assets/Scripts/TriggerArma/ColisionArma.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColisionArma : MonoBehaviour {
+
+    private AudioSource source;
+
+    public AudioClip blood;
+
+    // Use this for initialization
+    void Start () {
+
+        source = GetComponent<AudioSource>();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Weapon")
+
+        {
+            Debug.Log("pene");
+            source.PlayOneShot(blood, 0.3f);
+
+        }
+        
+    }
+
+}
