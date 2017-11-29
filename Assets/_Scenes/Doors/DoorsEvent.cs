d20ecbd4450387c8567a1daa8526656bfb313850
Clekio <alvaro.ortuno.m@gmail.com>
2017-11-29 03:53:44 +0100

    Animacion puertas

---
+++ Assets/_Scenes/Doors/DoorsEvent.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorsEvent : MonoBehaviour
+{
+    public Animator anim;
+
+    public OpenDoors inside;
+
+    // Use this for initialization
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (inside.inside == true)
+        {
+
+            anim.SetBool("Open", true);
+
+        }
+
+    }
+
+
+}
---
+++ Assets/_Scenes/Doors/OpenDoors.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenDoors : MonoBehaviour {
+
+    public bool inside = false;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            inside = true;
+
+        }
+
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            inside = false;
+
+        }
+
+    }
+
+
+}
