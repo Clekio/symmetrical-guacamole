cf9e417c70e264158213e37125cd13c8b1c09b24
guillemg15 <guillemg15@gmail.com>
2017-11-30 02:30:10 +0100

    GM_30112017
    Comentarios:
    - Arreglando el error absoluto

--- Assets/Scripts/DamageVida/LifeBar.cs
+++ Assets/Scripts/DamageVida/LifeBar.cs
@@ -5,36 +5,30 @@ using UnityEngine.UI;
 
 public class LifeBar : MonoBehaviour {
 
-    public Image currentHealth;
-    public Text ratioText;
-
     private float hitpoint = 150;
     private float maxHitpoint = 150;
-
-    private AudioSource source;
-
-    public AudioClip pain;
+	//edit guille
+	[Header("----- REFERENCIAS")]
+	public Image healthBar;
+	public Text ratioText;
 
 	// Use this for initialization
 	void Start () {
 
         UpdateHealthbar();
-        source = GetComponent<AudioSource>();
 
     }
 
 
     private void UpdateHealthbar()
     {
+		healthBar.fillAmount = hitpoint / maxHitpoint;
         float ratio = hitpoint / maxHitpoint;
-        currentHealth.rectTransform.localScale = new Vector3(ratio, 1, 1);
         ratioText.text = (ratio * 100).ToString("0") + '%';
     }
 
 	public void TakeDamage (float damage)
     {
-        source.PlayOneShot(pain, 0.2f);
-
         hitpoint -= damage;
         if (hitpoint < 0)
         {
@@ -57,11 +51,5 @@ public class LifeBar : MonoBehaviour {
 
         UpdateHealthbar();
     }
-
-
-	
-	// Update is called once per frame
-	void Update () {
 		
-	}
 }
