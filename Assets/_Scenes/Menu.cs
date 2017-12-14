000d8ab6e2a030176db1cf7bb6384ad98b359fdb
guillemg15 <guillemg15@gmail.com>
2017-12-14 04:06:41 +0100

    GM_PT1234
    SUICIDIO

---
+++ Assets/_Scenes/Menu.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour {
+
+	// Use this for initialization
+	public void LoadScene () {
+		SceneManager.LoadScene (1);
+	}
+	public void Quit () {
+		Application.Quit ();
+	}
+
+}
