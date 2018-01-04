04d4370ca9bbe653cf77daaee4db18adcacc00ad
Clekio <alvaro.ortuno.m@gmail.com>
2018-01-04 14:32:02 +0100

    Scripts chungos

---
+++ Assets/Scripts/Sense.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Sense : MonoBehaviour
+{
+
+    public float checkRadius;
+
+    public LayerMask checkLayers;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            // if only one, attack that one
+
+            Collider[] colliders = Physics.OverlapSphere(transform.position, checkRadius, checkLayers);
+            Array.Sort(colliders, new DistanceComparer(transform));
+
+            foreach(Collider item in colliders)
+            {
+                Debug.Log(item.name);
+            }
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(transform.position, checkRadius);
+
+
+
+    }
+
+
+}
