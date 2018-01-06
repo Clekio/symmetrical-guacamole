c6c24fc7eb16175bf2e3ecf242c059730d4f23d4
Clekio <alvaro.ortuno.m@gmail.com>
2018-01-06 17:48:50 +0100

    Cambios varios y comienzo de level design

---
+++ Assets/Prefabs Alvaro/Doors/CloseDoors.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloseDoors : MonoBehaviour
+{
+    public Animator anim;
+
+    public OpenDoors inside;
+
+    public DoorsEvent close;
+
+    // Use this for initialization
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    //void Update()
+    //{
+    //    if (inside.inside == true)
+    //    {
+
+    //        anim.Play("CloseDoor");
+
+
+    //    }
+
+    //}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+
+        {
+          
+            close.close = true;
+        }
+    }
+
+
+}
